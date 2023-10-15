79c299752b05dc07156e48492873711e9f8a771d
pdg1971 <146775941+pdg1971@users.noreply.github.com>
2023-10-15 17:07:56 +0100

    Acrescentar código para testar

--- Program.cs
+++ Program.cs
@@ -15,7 +15,12 @@ namespace Calculos
             Console.WriteLine($"4 + 2={Aritmetica.Somar(4,2)}");
             Console.WriteLine($"4 - 2={Aritmetica.Subtrair(4, 2)}");
 
-
+            double t1 = Conversoes.ConverterTemperatura(Conversoes.Conversaotemperatura.CelsiusFahrenheit, 36);
+            double t2 = Conversoes.ConverterTemperatura(Conversoes.Conversaotemperatura.FahrenheitCelsius, 100);
+            
+            Console.WriteLine($"36º Celsius = {t1}º Fahreneit");
+            Console.WriteLine($"100º Fahreneit = {t2}º Celsius");
+            
             Console.ReadKey();
         }
     }
